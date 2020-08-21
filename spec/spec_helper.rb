5bf19aa7f375fc500edf9a611243aa1b33ab9662
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-08-21 20:07:16 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+  
 ENV["SINATRA_ENV"] = "test"
 require_relative '../config/environment'
 require 'rack/test'
@@ -11,6 +12,7 @@ RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.order = 'default'
 end
+
 
 def app
   Rack::Builder.parse_file('config.ru').first
